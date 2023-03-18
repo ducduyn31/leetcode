bb9eae694c00fc9e7aac07fde8ae001829fb3d27
ducduyn31 <ducduy.ababat@gmail.com>
2023-03-19 01:25:33 +1100

    [14] Longest Common Prefix - Improved

--- easy/longest_common_prefix.py
+++ easy/longest_common_prefix.py
@@ -2,23 +2,33 @@ from typing import List
 
 
 def longestCommonPrefix(strs: List[str]) -> str:
-    if not strs or not strs[0]:
-        return ''
+    min_str_id, min_str = min(enumerate(strs), key=lambda pair: len(pair[1]))
+    strs.pop(min_str_id)
 
-    min_str = min(strs, key=len)
+    if len(strs) == 0:
+        return min_str
 
-    for i, c in enumerate(min_str):
-        for s in strs:
-            if s[i] != c:
-                return min_str[:i]
+    def share_prefix(prefix: str) -> bool:
+        return all([s[:len(prefix)] == prefix for s in strs])
 
-    return min_str
+    low, high = 0, len(min_str)
+    prefix_len = 0
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if share_prefix(min_str[:mid]):
+            low = mid + 1
+            prefix_len = mid
+        else:
+            high = mid - 1
+
+    return min_str[:prefix_len]
 
 
 if __name__ == '__main__':
-    print(longestCommonPrefix(["flower", "flow", "flight"]))
-    print(longestCommonPrefix(["dog", "racecar", "car"]))
-    print(longestCommonPrefix([]))
-    print(longestCommonPrefix([""]))
-    print(longestCommonPrefix(["a"]))
-    print(longestCommonPrefix(["ab", "a"]))
+    assert longestCommonPrefix(["ab", "a"]) == 'a'
+    assert longestCommonPrefix(["flower", "flow", "flight"]) == 'fl'
+    assert longestCommonPrefix(["dog", "racecar", "car"]) == ''
+    assert longestCommonPrefix([""]) == ''
+    assert longestCommonPrefix(["a"]) == 'a'
