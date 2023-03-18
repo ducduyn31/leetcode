5a0044bd6a0b81356fd5d5c6809534ca76c5980a
ducduyn31 <ducduy.ababat@gmail.com>
2023-03-19 03:17:55 +1100

    [567] Permutation in String - Clean and simple way

--- medium/permutation_str.py
+++ medium/permutation_str.py
@@ -1,32 +1,19 @@
-from collections import defaultdict
+from collections import Counter
 
 
 def checkInclusion(s1: str, s2: str) -> bool:
-    S = defaultdict(int)
-    nS = len(s1)
-    for c in s1:
-        S[c] += 1
+    term_counter = Counter(s1)
 
-    first = last = 0
-    while last < len(s2):
-        if s2[first] not in S:
-            first = last = first + 1
-        else:
-            if s2[last] in S and S[s2[last]] > 0:
-                S[s2[last]] -= 1
-                last += 1
-                nS -= 1
-            else:
-                S[s2[first]] += 1
-                first += 1
-                nS += 1
+    for i in range(0, len(s2) - len(s1) + 1):
+        count = Counter(s2[i:i + len(s1)])
 
-        if nS == 0:
+        if term_counter == count:
             return True
 
     return False
 
 
 if __name__ == '__main__':
-    print(checkInclusion('ab', 'eidbaooo'))
-    print(checkInclusion('ab', 'eidboaoo'))
+    assert checkInclusion('ab', 'eidbaooo') is True
+    assert checkInclusion('ab', 'eidboaoo') is False
+    assert checkInclusion('aabc', 'acaab') is True
