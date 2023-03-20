c6d8d6de4dc1d9c50b2f93cb5de2c15dee66b34d
ducduyn31 <ducduy.ababat@gmail.com>
2023-03-20 23:31:37 +1100

    [328] Odd Even Linked List

--- medium/odd_even_llist.py
+++ medium/odd_even_llist.py
@@ -18,23 +18,31 @@ class ListNode:
     def __str__(self):
         return self.get_chain_val()
 
+    def __eq__(self, other):
+        if not isinstance(other, ListNode) and other is not None:
+            return False
+        if self.val is None and other is None:
+            return True
+        return self.get_chain_val() == other.get_chain_val()
+
 
 class LinkedList:
     def __init__(self, nums: List[int]):
         self.first = self.from_list(nums)
 
     @staticmethod
-    def from_list(nums: List[int]) -> ListNode:
-        first = ListNode(nums[0])
-        current = first
-        n = len(nums)
-
-        for _ in range(n - 1):
-            current.next = ListNode(nums[1])
-            nums.pop(0)
-            current = current.next
-
-        return first
+    def from_list(nums: List[int]) -> 'ListNode':
+        head = None
+        current = None
+        for i, n in enumerate(nums):
+            if i == 0:
+                head = ListNode(n)
+                current = head
+            else:
+                next = ListNode(n)
+                current.next = next
+                current = next
+        return head
 
     def __str__(self):
         l = []
@@ -49,32 +57,30 @@ class LinkedList:
 
 
 def oddEvenList(head: ListNode) -> ListNode:
-    if not head:
+    if not head or not head.next:
         return head
 
-    odd = last_odd = head
-    current = even = last_even = head.next
-    isOdd = True
+    odd_head = odd_tail = head
+    even_head = even_tail = head.next
 
-    while current:
-        if isOdd:
-            last_odd.next = current = current.next
-            if last_odd.next:
-                last_odd = last_odd.next
-            isOdd = False
-        elif last_even:
-            last_even.next = current = current.next
-            last_even = last_even.next
-            isOdd = True
+    while odd_tail.next and even_tail.next:
+        if even_tail.next:
+            odd_tail.next = odd_tail.next.next
 
-    last_odd.next = even
+        if odd_tail.next:
+            even_tail.next = even_tail.next.next
 
-    return odd
+        odd_tail = odd_tail.next
+        even_tail = even_tail.next
+
+    odd_tail.next = even_head
+
+    return odd_head
 
 
 if __name__ == '__main__':
-    print(oddEvenList(LinkedList.from_list([])))
-    print(oddEvenList(LinkedList.from_list([1, 2, 3, 4, 5, 6, 7, 8])))
-    print(oddEvenList(LinkedList.from_list([1])))
-    print(oddEvenList(LinkedList.from_list([1, 2, 3, 4, 5])))
-    print(oddEvenList(LinkedList.from_list([2, 1, 3, 5, 6, 4, 7])))
+    assert oddEvenList(LinkedList.from_list([])) is None
+    assert oddEvenList(LinkedList.from_list([1, 2, 3, 4, 5, 6, 7, 8])) == LinkedList.from_list([1, 3, 5, 7, 2, 4, 6, 8])
+    assert oddEvenList(LinkedList.from_list([1])) == LinkedList.from_list([1])
+    assert oddEvenList(LinkedList.from_list([1, 2, 3, 4, 5])) == LinkedList.from_list([1, 3, 5, 2, 4])
+    assert oddEvenList(LinkedList.from_list([2, 1, 3, 5, 6, 4, 7])) == LinkedList.from_list([2, 3, 6, 7, 1, 5, 4])
