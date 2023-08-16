1dc7fe0e16f06bd6eb07b367ba97998898c4665f
ducduyn31 <ducduy.ababat@gmail.com>
2023-08-16 19:40:45 +1000

    [239] Sliding Window Maximum

--- hard/sliding_window_maximum.py
+++ hard/sliding_window_maximum.py
@@ -1,33 +1,48 @@
-from collections import deque
+import unittest
 from typing import List
+from collections import deque
 
 
-def maxSlidingWindow(nums: List[int], k: int) -> List[int]:
-    slider = deque()
-    ans = []
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if k == len(nums):
+            return [max(nums)]
+        result = []
+        index_max = deque()
 
-    for i in range(k):
-        while slider and nums[slider[-1]] <= nums[i]:
-            slider.pop()
-        slider.append(i)
+        for i in range(len(nums)):
+            while index_max and nums[index_max[-1]] < nums[i]:
+                index_max.pop()
+            index_max.append(i)
+            if i >= k - 1:
+                result.append(nums[index_max[0]])
 
-    for i in range(k, len(nums)):
-        ans.append(nums[slider[0]])
-        while slider and slider[0] <= i - k:
-            slider.popleft()
-        while slider and nums[slider[-1]] <= nums[i]:
-            slider.pop()
-        slider.append(i)
+            while index_max and i - k + 1 >= index_max[0]:
+                index_max.popleft()
 
-    ans.append(nums[slider[0]])
-
-    return ans
+        return result
 
 
-if __name__ == '__main__':
-    print(maxSlidingWindow(nums=[1, 3, 1, 2, 0, 5], k=3))
-    print(maxSlidingWindow(nums=[1, 3, -1, -3, 5, 3, 6, 7], k=3))
-    print(maxSlidingWindow(nums=[1], k=1))
-    print(maxSlidingWindow(nums=[1, -1], k=1))
-    print(maxSlidingWindow(nums=[9, 11], k=2))
-    print(maxSlidingWindow(nums=[4, -2], k=2))
+class SolutionTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.max_sliding_window = Solution().maxSlidingWindow
+
+    def test_max_sliding_window_should_for_k3_and_len_8(self):
+        nums = [1, 3, -1, -3, 5, 3, 6, 7]
+        k = 3
+        self.assertEqual([3, 3, 5, 5, 6, 7], self.max_sliding_window(nums, k))
+
+    def test_max_sliding_window_should_for_k1_and_len_1(self):
+        nums = [1]
+        k = 1
+        self.assertEqual([1], self.max_sliding_window(nums, k))
+
+    def test_max_sliding_window_should_for_k1_and_len_2(self):
+        nums = [1, -1]
+        k = 1
+        self.assertEqual([1, -1], self.max_sliding_window(nums, k))
+
+    def test_max_sliding_window_should_for_k3_and_len_6(self):
+        nums = [1, 3, 1, 2, 0, 5]
+        k = 3
+        self.assertEqual([3, 3, 2, 5], self.max_sliding_window(nums, k))
