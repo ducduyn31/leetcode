c02f4cb87bc1a8deda532fd03b975146e4ef845e
ducduyn31 <ducduy.ababat@gmail.com>
2023-10-05 04:18:17 +1100

    [486] Predict the winner Space Optimized

--- medium/predict_the_winner.py
+++ medium/predict_the_winner.py
@@ -5,16 +5,20 @@ from typing import List
 
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        @cache
-        def dp(turn, start, end):
-            if start == end:
-                return nums[end]
-            return max(
-                nums[end] - dp(turn + 1, start, end - 1),
-                nums[start] - dp(turn + 1, start + 1, end)
-            )
+        n = len(nums)
+        dp = [0] * n
 
-        return dp(0, 0, len(nums) - 1) >= 0
+        for i in range(n):
+            for j in range(n - i):
+                if i == 0:
+                    dp[j] = nums[j]
+                else:
+                    dp[j] = max(
+                        nums[i + j] - dp[j],
+                        nums[j] - dp[j + 1],
+                    )
+
+        return dp[0] >= 0
 
 
 class SolutionTest(unittest.TestCase):
@@ -26,3 +30,6 @@ class SolutionTest(unittest.TestCase):
 
     def test_2(self):
         self.assertEqual(self.predict_the_winner([1, 5, 233, 7]), True)
+
+    def test_3(self):
+        self.assertEqual(self.predict_the_winner([2, 4, 55, 6, 8]), False)
