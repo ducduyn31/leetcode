2b99a0e8e42a85c372cd489b8ace3d5ff8108756
ducduyn31 <ducduy.ababat@gmail.com>
2024-04-11 19:13:46 +1000

    402. Remove K Digits

--- medium/remove_k_digits.py
+++ medium/remove_k_digits.py
@@ -1,23 +1,64 @@
-def removeKdigits(num: str, k: int) -> str:
-    result = []
+import unittest
 
-    for c in num:
-        while k and result and c < result[-1]:
-            result.pop()
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        if k >= len(num):
+            return "0"
+        stack = []
+
+        for c in num:
+            if not stack:
+                stack.append(c)
+            else:
+                while stack and ord(stack[-1]) > ord(c) and k > 0:
+                    stack.pop()
+                    k -= 1
+                stack.append(c)
+
+        while k > 0:
+            stack.pop()
             k -= 1
-        result.append(c)
 
-    while k:
-        result.pop()
-        k -= 1
-
-    return ''.join(result).lstrip('0') or '0'
+        return "".join(stack).lstrip("0") or "0"
 
 
-if __name__ == '__main__':
-    print(removeKdigits('9', 1))
-    print(removeKdigits('43214321', 4))
-    print(removeKdigits('112', 1))
-    print(removeKdigits('1432219', 3))
-    print(removeKdigits('10200', 1))
-    print(removeKdigits('10', 2))
+class SolutionTest(unittest.TestCase):
+
+    def setUp(self):
+        self.remove_kdigits = Solution().removeKdigits
+
+    def test_1(self):
+        num = "1432219"
+        k = 3
+        self.assertEqual(self.remove_kdigits(num, k), "1219")
+
+    def test_2(self):
+        num = "10200"
+        k = 1
+        self.assertEqual(self.remove_kdigits(num, k), "200")
+
+    def test_3(self):
+        num = "10"
+        k = 2
+        self.assertEqual(self.remove_kdigits(num, k), "0")
+
+    def test_4(self):
+        num = "112"
+        k = 1
+        self.assertEqual(self.remove_kdigits(num, k), "11")
+
+    def test_5(self):
+        num = "1234567890"
+        k = 9
+        self.assertEqual(self.remove_kdigits(num, k), "0")
+
+    def test_6(self):
+        num = "1432219"
+        k = 4
+        self.assertEqual(self.remove_kdigits(num, k), "119")
+
+    def test_7(self):
+        num = "52660469"
+        k = 2
+        self.assertEqual(self.remove_kdigits(num, k), "260469")
